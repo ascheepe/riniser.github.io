fd156069bba7526e3155eb8c9ea9b3c3a98ecfc6
Axel Scheepers <no@spam.please>
2026-03-05 18:57:35 +0100

    reset resolution warning and add an idicator to show how bad the image is

--- riniser.js
+++ riniser.js
@@ -1,10 +1,12 @@
 function resizeImage() {
+  /* 150dpi should really be the minimum */
   const MIN_WIDTH = 1240;
   const MIN_HEIGHT = 1754;
 
   const input = document.getElementById("image-input");
   const output = document.getElementById("resized-image");
   const downloadButton = document.getElementById("download-button");
+  const resolutionWarning = document.getElementById("resolution-warning");
 
   downloadButton.style.display = "none";
 
@@ -23,8 +25,16 @@ function resizeImage() {
     let height = img.height;
 
     if (width < MIN_WIDTH || height < MIN_HEIGHT) {
-      document.getElementById("resolution-warning").textContent =
-        "⚠ Afbeelding is eigenlijk te klein om te printen!";
+      var quality = 0;
+      if (width < MIN_WIDTH) {
+        quality = Math.round(100 * width / MIN_WIDTH);
+      } else {
+        quality = Math.round(100 * height / MIN_HEIGHT);
+      }
+      resolutionWarning.textContent =
+        "⚠ Afbeelding is eigenlijk te klein om te printen! (" + quality + "% ok)";
+    } else {
+      resolutionWarning.textContent = "";
     }
 
     if (width < height) {
